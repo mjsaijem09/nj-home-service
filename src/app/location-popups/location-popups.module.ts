295bb837fa90e2eceee39248b7aa1fba76cc5e57
mjsaijem09 <mejiasmark9@gmail.com>
2023-07-04 20:55:54 +0800

    transfered directive to module

--- src/app/location-popups/location-popups.module.ts
+++ src/app/location-popups/location-popups.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
-import { GooglePlacesDirective } from 'src/app/directives/google-places.directive';
 import { AskToChageLocationComponent } from './ask-to-chage-location/ask-to-chage-location.component';
 import { LocationSelectionPopupComponent } from './location-selection-popup/location-selection-popup.component';
 import { GetCurrentLocationComponent } from './get-current-location/get-current-location.component';
@@ -15,7 +14,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
         AskToChageLocationComponent,
         LocationSelectionPopupComponent,
         GetCurrentLocationComponent,
-        GooglePlacesDirective
     ],
     imports: [
         CommonModule,
@@ -28,7 +26,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
         AskToChageLocationComponent,
         LocationSelectionPopupComponent,
         GetCurrentLocationComponent,
-        GooglePlacesDirective
     ]
 })
 export class LocationPopupsModule { }
