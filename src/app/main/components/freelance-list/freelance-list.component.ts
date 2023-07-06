cb9221a1da7b1f03e52df1c07a53f2089eeb3e77
ITteam2023 <teambookusit@gmail.com>
2023-07-06 10:45:09 +0800

    filter update with filter api

--- src/app/main/components/freelance-list/freelance-list.component.ts
+++ src/app/main/components/freelance-list/freelance-list.component.ts
@@ -1,44 +1,75 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { ApiServicesService } from 'src/app/services/api-services.service';
 import { LocationService } from 'src/app/services/location.service';
-
+import { SearchService } from 'src/app/services/search.service';
 @Component({
   selector: 'app-freelance-list',
   templateUrl: './freelance-list.component.html',
   styleUrls: ['./freelance-list.component.scss']
 })
 export class FreelanceListComponent implements OnInit {
-  @Input() title: string;
-  @Input() category: string;
-  @Input() sort: string;
   isLoading:boolean = false;
   location
-
+  filterURL = 'therapist/search?';
   constructor(
     private _http: ApiServicesService,
-    private _locationService: LocationService
+    private _locationService: LocationService,
+    private _search: SearchService
     ) { }
 
   ngOnInit(): void {
-    console.log(this.title, this.category);
     this.location = JSON.parse(localStorage.getItem('location-radius'));
+    this.initLocationParams();
     this.initList();
-    this._locationService.get_location().subscribe(
-      res => {
-        this.location = res;
-        console.log(this.location);
-      },
-      err => {
-        console.log(err);
+    this._search.getUserSearch().subscribe(
+      (res: any) => {
+        this.filterURL = 'therapist/search?';
+        if (res.text) {
+          this.updateQueryParam('search', res.text);
+        }
+        if (res.gender) {
+          this.updateQueryParam('gender', res.gender);
+        }
+        if (res.sortBy) {
+          this.updateQueryParam(res.sortBy, 'true');
+        }
+        if (res.category) {
+          this.updateQueryParam('category', res.category);
+        }
+        this.initLocationParams();
+        this.initList();
       }
-    )
+    );
+  }
+  initLocationParams() {
+    if (this.location.distanceRadius) {
+      this.updateQueryParam('dis', this.location.distanceRadius.toString());
+    }
+    if (this.location.coordinates) {
+      this.updateQueryParam('lat', this.location.coordinates.lat);
+      this.updateQueryParam('long', this.location.coordinates.lng);
+    }
+  }
+  private updateQueryParam(key: string, value: string): void {
+    const params = new URLSearchParams(this.filterURL);
+    if (params.has(key)) {
+      params.delete(key);
+    }
+    params.append(key, value);
+    const updatedFilterURL = params.toString();
+    this.filterURL = decodeURIComponent(updatedFilterURL);
+
+    // Remove leading `=&` if present after `/search?`
+    this.filterURL = this.filterURL.replace(/\/search\?=&/, '/search?');
+    console.log(this.filterURL);
   }
 
+// therapist/search?category=5fc739a0f9632400073b6ea8&lat=10.315699&long=123.885437&nearest=true&dis=&search=jesvir&gender=male
   list = [];
   initList() {
     this.isLoading = true;
-    this._http.get_nj(`therapist`).subscribe(
+    this._http.get_nj(this.filterURL).subscribe(
       res => {
         console.log(res);
         this.list = res.result
@@ -49,4 +80,5 @@ export class FreelanceListComponent implements OnInit {
       }
     )
   }
+  
 }
